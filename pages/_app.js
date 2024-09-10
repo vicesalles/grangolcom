88523247770656f862f9872b529c530406f4795e
vicesalles <vicenc.salles@gmail.com>
2024-09-10 13:25:14 +0200

    Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,15 @@
 import { appWithTranslation } from 'next-i18next';
 import nextI18NextConfig from '../next-i18next.config';
-import RootLayout from '../components/layout';
 import ConsentBanner from '../components/ConsentBanner';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
-import { useEffect } from 'react';
 
-import { checkConsent, loadGTM } from '../utils/consent'; // Import helper functions
+
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    const consentGiven = checkConsent(); // Your existing consent logic
-    if (consentGiven) {
-      // Load Google Analytics
-      const script = document.createElement('script');
-      script.src = `https://www.googletagmanager.com/gtag/js?id=G-WSSRG343P3`;
-      script.async = true;
-      document.head.appendChild(script);
-
-      // Initialize gtag after script is loaded
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){window.dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-WSSRG343P3');
-
-      // Notify Google Analytics about consent
-      gtag('consent', 'update', {
-        'ad_storage': 'denied',       // 'denied' if consent is not given
-        'analytics_storage': 'granted' // 'denied' if consent is not given
-      });
-    }
-  }, []);
   
-  return <><Component {...pageProps} /><ConsentBanner /></>;
+  return <><Component {...pageProps} /><ConsentBanner /><Analytics /></>;
 }
 
 export default appWithTranslation(MyApp, nextI18NextConfig);
