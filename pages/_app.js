05217516dfa5d17e20c1261d5eb115dd870dac3a
vicesalles <vicenc.salles@gmail.com>
2024-11-05 11:48:43 +0100

    Speed Insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { appWithTranslation } from 'next-i18next';
 import nextI18NextConfig from '../next-i18next.config';
 import ConsentBanner from '../components/ConsentBanner';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css';
 import '../i18n';  // Import the i18n initialization
 
@@ -9,7 +10,7 @@ import '../i18n';  // Import the i18n initialization
 function MyApp({ Component, pageProps }) {
 
   
-  return <><Component {...pageProps} /><ConsentBanner /><Analytics /></>;
+  return <><Component {...pageProps} /><ConsentBanner /><Analytics /><SpeedInsights /></>;
 }
 
 export default appWithTranslation(MyApp, nextI18NextConfig);
