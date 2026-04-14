a8ee531d35af56475ac628009b0d636b8669382b
Vice Sallés <vicenc.salles@gmail.com>
2026-04-14 19:21:48 +0200

    preparo el text per publicar els equips

--- pages/api/teams/[slug].js
+++ pages/api/teams/[slug].js
@@ -1,41 +1,41 @@
 // /pages/api/teams/[slug].js
 const teams = {
   en: [
-    { slug: "vapor", name: "CE El Vapor", mainColor: "black", secondColor: "white", description: "English description", mainPlayer: "Fàtima (8)" },
-    { slug: "olivera", name: "CF Olivera", mainColor: "green", secondColor: "white", description: "English description", mainPlayer: "Aina (6)" },
-    { slug: "miner", name: "Club Atlètic Miner", mainColor: "yellow", secondColor: "black", description: "English description", mainPlayer: "Otger (10)" },
-    { slug: "victoria", name: "FC Victòria", mainColor: "black", secondColor: "white", description: "English description", mainPlayer: "Martí (3)"},
-    { slug: "nautic", name: "Club Nàutic", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Ot (8)"},
-    { slug: "dos_fars", name: "Dos Fars Club Esportiu", mainColor: "blue", secondColor: "yellow", description: "English description", mainPlayer: "Narcís (2)"},
-    { slug: "pescador", name: "CF Pescador", mainColor: "white", secondColor: "blue", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "nou_vapor", name: "CE Nou Vapor", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "politecnic", name: "CE Institut Politècnic", mainColor: "red", secondColor: "black", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "guilla", name: "La Guilla CF", mainColor: "orange", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"}
+    { slug: "vapor", name: "CE El Vapor", mainColor: "black", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Fàtima (8)" },
+    { slug: "olivera", name: "CF Olivera", mainColor: "green", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Aina (6)" },
+    { slug: "miner", name: "Club Atlètic Miner", mainColor: "yellow", secondColor: "black", description: "English description", rivals:"", mainPlayer: "Otger (10)" },
+    { slug: "victoria", name: "FC Victòria", mainColor: "black", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Martí (3)"},
+    { slug: "nautic", name: "Club Nàutic", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Ot (8)"},
+    { slug: "dos_fars", name: "Dos Fars Club Esportiu", mainColor: "blue", secondColor: "yellow", description: "English description", rivals:"", mainPlayer: "Narcís (2)"},
+    { slug: "pescador", name: "CF Pescador", mainColor: "white", secondColor: "blue", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "nou_vapor", name: "CE Nou Vapor", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: ""},
+    { slug: "politecnic", name: "CE Institut Politècnic", mainColor: "red", secondColor: "black", description: "English description", rivals:"", mainPlayer: ""},
+    { slug: "guilla", name: "La Guilla CF", mainColor: "orange", secondColor: "white", description: "English description", rivals:"", mainPlayer: ""}
   ],
   ca: [
-    { slug: "vapor", name: "CE El Vapor", mainColor: "black", secondColor: "white", description: "Descripció en Català", mainPlayer: "Fàtima (8)" },
-    { slug: "olivera", name: "CF Olivera", mainColor: "green", secondColor: "white", description: "Descripció en Català", mainPlayer: "Aina (6)" },
-    { slug: "miner", name: "Club Atlètic Miner", mainColor: "yellow", secondColor: "black", description: "English description", mainPlayer: "Otger (10)"},
-    { slug: "victoria", name: "FC Victòria", mainColor: "black", secondColor: "white", description: "English description", mainPlayer: "Martí (3)"},
-    { slug: "nautic", name: "Club Nàutic", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Ot (8)"},
-    { slug: "dos_fars", name: "Dos Fars Club Esportiu", mainColor: "yellow", secondColor: "blue", description: "English description", mainPlayer: "Narcís (2)"},
-    { slug: "pescador", name: "CF Pescador", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "nou_vapor", name: "CE Nou Vapor", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "politecnic", name: "CE Institut Politècnic", mainColor: "red", secondColor: "black", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "guilla", name: "La Guilla CF", mainColor: "orange", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"}
+    { slug: "vapor", name: "CE El Vapor", mainColor: "black", secondColor: "white", description: "Descripció en Català", rivals:"", mainPlayer: "Fàtima (8)" },
+    { slug: "olivera", name: "CF Olivera", mainColor: "green", secondColor: "white", description: "Descripció en Català", rivals:"", mainPlayer: "Aina (6)" },
+    { slug: "miner", name: "Club Atlètic Miner", mainColor: "yellow", secondColor: "black", description: "English description", rivals:"", mainPlayer: "Otger (10)"},
+    { slug: "victoria", name: "FC Victòria", mainColor: "black", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Martí (3)"},
+    { slug: "nautic", name: "Club Nàutic", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Ot (8)"},
+    { slug: "dos_fars", name: "Dos Fars Club Esportiu", mainColor: "yellow", secondColor: "blue", description: "English description", rivals:"", mainPlayer: "Narcís (2)"},
+    { slug: "pescador", name: "CF Pescador", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "nou_vapor", name: "CE Nou Vapor", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "politecnic", name: "CE Institut Politècnic", mainColor: "red", secondColor: "black", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "guilla", name: "La Guilla CF", mainColor: "orange", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Marina (1)"}
     
   ],
   es: [
-    { slug: "vapor", name: "CE El Vapor", mainColor: "black", secondColor: "white", description: "Descripción en Español", mainPlayer: "Fàtima (8)" },
-    { slug: "olivera", name: "CF Olivera", mainColor: "green", secondColor: "white", description: "Descripción en Español", mainPlayer: "Aina (6)" },
-    { slug: "miner", name: "Club Atlètic Miner", mainColor: "yellow", secondColor: "black", description: "English description", mainPlayer: "Otger (10)"},
-    { slug: "victoria", name: "FC Victòria", mainColor: "black", secondColor: "white", description: "English description", mainPlayer: "Martí (3)"},
-    { slug: "nautic", name: "Club Nàutic", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Ot (8)"},
-    { slug: "dos_fars", name: "Dos Fars Club Esportiu", mainColor: "blue", secondColor: "yellow", description: "English description", mainPlayer: "Narcís (2)"},
-    { slug: "pescador", name: "CF Pescador", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "nou_vapor", name: "CE Nou Vapor", mainColor: "blue", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "politecnic", name: "CE Institut Politècnic", mainColor: "red", secondColor: "black", description: "English description", mainPlayer: "Marina (1)"},
-    { slug: "guilla", name: "La Guilla CF", mainColor: "orange", secondColor: "white", description: "English description", mainPlayer: "Marina (1)"}
+    { slug: "vapor", name: "CE El Vapor", mainColor: "black", secondColor: "white", description: "Descripción en Español", rivals:"", mainPlayer: "Fàtima (8)" },
+    { slug: "olivera", name: "CF Olivera", mainColor: "green", secondColor: "white", description: "Descripción en Español", rivals:"", mainPlayer: "Aina (6)" },
+    { slug: "minero", name: "Club Atlètic Miner", mainColor: "yellow", secondColor: "black", description: "English description", rivals:"", mainPlayer: "Otger (10)"},
+    { slug: "victoria", name: "FC Victòria", mainColor: "black", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Martí (3)"},
+    { slug: "nautico", name: "Club Nàutic", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Ot (8)"},
+    { slug: "dos_faros", name: "Dos Fars Club Esportiu", mainColor: "blue", secondColor: "yellow", description: "English description", rivals:"", mainPlayer: "Narcís (2)"},
+    { slug: "pescador", name: "CF Pescador", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "nuevo_vapor", name: "CE Nou Vapor", mainColor: "blue", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "politecnico", name: "CE Institut Politècnic", mainColor: "red", secondColor: "black", description: "English description", rivals:"", mainPlayer: "Marina (1)"},
+    { slug: "zorros", name: "La Guilla CF", mainColor: "orange", secondColor: "white", description: "English description", rivals:"", mainPlayer: "Marina (1)"}
   ]
 };
 
